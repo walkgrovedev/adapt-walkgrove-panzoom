c124b402940c17bdb0c8b83ddf79356d6dd34b02
@laura.lululand <laura.lululand@gmail.com>
2021-08-17 10:46:22 +0100

    update functionality to lock naviagtion until movement xcomplete and to allow a new image to fade over the top (from AssetWisdom)

--- js/adapt-walkgrove-panzoom.js
+++ js/adapt-walkgrove-panzoom.js
@@ -15,6 +15,7 @@ define([
     _stageIndex:-1,
     _panX: 50,
     _panY: 50,
+    _currentTop: 0,
     
     preRender: function() {
       this.checkIfResetOnRevisit();
@@ -23,8 +24,9 @@ define([
     postRender: function() {
       this.setReadyStatus();
 
-      if(screen.width <= 520) {
-        this.$('.panzoom__instruction').html(this.model.get('mobileInstructions'));
+      if(screen.width <= 768) {
+        this.$('.panzoom__instruction').html(this.model.get('mobileInstruction'));
+        this.setCompletionStatus();
       }
 
       this.$('.panzoom__images').css({'max-width': this.model.get('_graphic')._width + 'px', 'max-height': this.model.get('_graphic')._height + 'px'})
@@ -41,26 +43,43 @@ define([
 
     onPrev: function() {
       if(this._stageIndex > 0) {
+        this.$('.panzoom__image-fade').eq(this._stageIndex).addClass('hide');
         this._stageIndex--;
 
-        this.setStageVisible();
-        this.zoomMap();
-        this.checkControls();
+        this.disableControls();
+
+        _.delay(function () {
+          this.setStageVisible();
+          this.zoomMap();
+          //this.checkControls();
+          this.$('.panzoom__image-fade').removeClass('show');
+          this.$('.panzoom__image-fade').removeClass('hide');
+        }.bind(this), 100);
         
         this.$('.panzoom__widget-content').eq(this._stageIndex).a11y_focus();
+      } else {
+        this.StartEndStage(true);
       }
     },
 
     onNext: function() {
-      if(this._stageIndex <this.model.get('_items').length-1) {
+      if(this._stageIndex < this.model.get('_items').length-2) {
+        this.$('.panzoom__image-fade').eq(this._stageIndex).addClass('hide');
         this._stageIndex++;
 
-        this.setStageVisible();
-        this.zoomMap();
-        this.checkControls();
+        this.disableControls();
+
+        _.delay(function () {
+          this.setStageVisible();
+          this.zoomMap();
+          //this.checkControls();
+          this.$('.panzoom__image-fade').removeClass('show');
+          this.$('.panzoom__image-fade').removeClass('hide');
+        }.bind(this), 100);
 
         this.$('.panzoom__widget-content').eq(this._stageIndex).a11y_focus();
-
+      } else {
+        this.StartEndStage(false); 
         if(this._stageIndex === this.model.get('_items').length-1) {
           this.setCompletionStatus();
           this.$('.panzoom__instruction').html(this.model.get('endInstructions'));
@@ -80,8 +99,13 @@ define([
       });
     },
 
+    disableControls: function() {
+      this.$('.js-panzoom-prev').addClass('is-disabled');
+      this.$('.js-panzoom-next').addClass('is-disabled');
+    },
+
     checkControls: function() {
-      if(this._stageIndex === 0 ) {
+      if(this._stageIndex === -1 ) {
         this.$('.js-panzoom-prev').addClass('is-disabled');
       } else {
         this.$('.js-panzoom-prev').removeClass('is-disabled');
@@ -95,7 +119,56 @@ define([
 
     zoomMap: function() {
       const map = this.$('.panzoom__image');
-      map.animate({ left: this.panX + '%', top: this.panY + 'rem' }, 1000);
+      const mapFade = this.$('.panzoom__image-fade');
+
+      this.$('.panzoom__image').addClass('scaleDown');
+
+      if(this._stageIndex === -1 || this._stageIndex === this.model.get('_items').length-1) {
+        
+        map.animate({ left: '0%' }, 1000, () => { //top: this.panY + 'rem'
+          this._currentTop = '0%';
+          map.css('-webkit-transform','scale(1) translate(0%, 0%)');
+          //mapFade.css('-webkit-transform','scale(1) translate(0%, 0%)');
+          _.delay(function () {
+            //mapFade.eq(this._stageIndex).addClass('show'); 
+            this.checkControls();
+          }.bind(this), 1000);
+        });
+        mapFade.animate({ left: '0%' }, 1000);
+        $({top: this._currentTop}).animate({top: '0%'}, {
+          duration: 1000,
+          easing: 'linear',
+          step: function() {
+              map.css('-webkit-transform','scale(1) translate(0%, 0%)');
+              //mapFade.css('-webkit-transform','scale(2) translate(0%, ' + this.top + '%)');
+          }
+        });
+
+        
+      } else {
+
+        this.$('.panzoom__image').removeClass('scaleDown');
+
+        map.animate({ left: this.panX + '%' }, 1000, () => { //top: this.panY + 'rem'
+          this._currentTop = this.panY;
+          map.css('-webkit-transform','scale(2) translate(0%, ' + this._currentTop + '%)');
+          mapFade.css('-webkit-transform','scale(2) translate(0%, ' + this._currentTop + '%)');
+          _.delay(function () {
+            mapFade.eq(this._stageIndex).addClass('show'); 
+            this.checkControls();
+          }.bind(this), 1000);
+        });
+        mapFade.animate({ left: this.panX + '%' }, 1000);
+        $({top: this._currentTop}).animate({top: this.panY}, {
+          duration: 1000,
+          easing: 'linear',
+          step: function() {
+              map.css('-webkit-transform','scale(2) translate(0%, ' + this.top + '%)');
+              //mapFade.css('-webkit-transform','scale(2) translate(0%, ' + this.top + '%)');
+          }
+        });
+
+      }
     },
 
     onStart: function() {
@@ -105,9 +178,31 @@ define([
       this.$('.panzoom__image-small').addClass('is-visible');
 
       this.$('.panzoom__image').addClass('scale');
-      setTimeout(()=>{ this.onNext(); }, 1500);
-    }
+      setTimeout(()=>{ this.onNext(); }, 1000);
+    },
 
+    StartEndStage: function(_start) {
+      this.$('.panzoom__image-fade').eq(this._stageIndex).addClass('hide');
+
+      this.disableControls();
+
+      if(_start) {
+        this._stageIndex--;
+        //this.$('.panzoom__image').css('-webkit-transform','scale(1) translate(0%, 0%)');
+      } else {
+        this._stageIndex++;
+        //this.$('.panzoom__image').css('-webkit-transform','scale(1) translate(-25%, 0%)');
+      }
+
+      _.delay(function () {
+        this.setStageVisible();
+        this.zoomMap();
+        //this.checkControls();
+        this.$('.panzoom__image-fade').removeClass('show');
+        this.$('.panzoom__image-fade').removeClass('hide');
+      }.bind(this), 100);
+
+    }
 
 
   },
